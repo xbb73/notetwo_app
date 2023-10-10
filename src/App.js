f552d7c989aedf1a49973e0fd4af613d270f22fe
xbb73 <xbb73@outlook.com>
2023-10-10 11:32:20 -0400

    fix2

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ const App = ({ signOut }) => {
   }
 
   return (
+    
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
@@ -90,17 +91,17 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
         </Flex>
       </View>
-      <View
-        name="image"
-        as="input"
-        type="file"
-        style={{ alignSelf: "end" }}
-      />
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
       {notes.map((note) => (
